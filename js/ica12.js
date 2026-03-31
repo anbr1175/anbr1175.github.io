7c4abfeaae4e53d3b276ecde413a467578efbc6a
Annie Bronson <anbr1175@colorado.edu>
2026-03-31 10:08:02 -0600

    new

---
+++ js/ica12.js
@@ -0,0 +1,9 @@
+let newBtn = document.querySelector('#js-new-quote');
+
+newBtn.addEventListener('click', getQuote);
+
+const endpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion';
+
+function getQuote() {
+   // alert("THIS WORKS!");
+}
