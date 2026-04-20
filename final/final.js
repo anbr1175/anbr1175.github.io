a11ade3a70d1d8c1938f708ef8aef78a08f0ddc3
Annie Bronson <anbr1175@colorado.edu>
2026-04-20 11:08:36 -0600

    edits

--- final/final.js
+++ final/final.js
@@ -1,5 +1,7 @@
 const volumeValue = document.getElementById("volumeValue");
 const gameArea = document.getElementById("gameArea");
+const endButton = document.getElementById("endButton");
+const finalMessage = document.getElementById("finalMessage");
 
 
 let volume = 50;
