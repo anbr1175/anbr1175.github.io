326d0f90d0ce401a2cb97ba6b6f3d879bf33d9b1
Annie Bronson <anbr1175@colorado.edu>
2026-04-20 11:22:26 -0600

    Update final.js

--- final/final.js
+++ final/final.js
@@ -5,3 +5,23 @@ const finalMessage = document.getElementById("finalMessage");
 
 
 let volume = 50;
+let gameRunning = true;
+let spawnInterval;
+
+const changeOptions = [1,1,5,14,17,-19,30,-31,11,-14,-7,8]
+
+function spawnButton() {
+    if (!gameRunning) return;
+
+    const btn = document.getElementById("button");
+    btn.classList.add("mole-btn");
+
+    const randomChange = changeOptions[Math.floor(Math.random() * changeOptions.length)];
+    btn.textContent = randomChange > 0 ? `+${randomChange}` : `${randomChange}`;
+}
+
+spawnInterval = setInterval(() => {
+    spawnButton();
+    spawnButton();
+    spawnButton();
+},700);
